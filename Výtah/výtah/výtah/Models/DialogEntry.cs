a90af9bae09b2c46a7a648d95d43e794e41a52d9
Hoxret <61802hotarek@student.sstebrno.cz>
2026-05-21 06:38:08 +0200

    Delete Výtah/výtah directory

--- Výtah/výtah/výtah/Models/DialogEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vytah.Models
-{
-    public class DialogEntry
-    {
-        public string Speaker { get; set; }   // "Paní", "Muž", "Holčička", "" = narativ
-        public string Text { get; set; }
-        public string NextSceneId { get; set; }  // volitelné: přejdi do jiné scény po dialogu
-    }
-}
